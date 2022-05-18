a3f8cb35f3b3ebabb5e01d1cbd158be3b5c5ee02
Boris Tereshko <tbm_mail@tut.by>
2022-05-18 13:14:24 +0300

    feat: create 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const fs = require('fs');
+const { stdout } = process;
 
 const stream = fs.ReadStream(path.resolve(__dirname, 'text.txt'), 'utf-8');
 
 let data = '';
 
 stream.on('data', chunk => data += chunk);
-stream.on('end', () => console.log(data));
+stream.on('end', () => stdout.write(data));
 stream.on('error', error => console.log('Error', error.message));
