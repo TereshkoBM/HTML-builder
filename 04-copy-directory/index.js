d1994694887c14f480e0770392baab8848e1bfdd
Boris Tereshko <tbm_mail@tut.by>
2022-05-19 14:08:14 +0300

    feat: create 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require("fs");
+const path = require("path");
 
+fs.mkdir(path.resolve(__dirname, "files-copy"), { recursive: true }, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Копирую файлы из папки files в папку files-copy");
+  fs.readdir(
+    path.resolve(__dirname, "files"),
+    { withFileTypes: true },
+    (err, files) => {
+      if (err) console.log(err);
+      else {
+        files.forEach((file) => {
+          if (file.isFile()) {
+            fs.copyFile(
+              path.resolve(__dirname, "files", file.name),
+              path.resolve(__dirname, "files-copy", file.name),
+              (err) => {
+                if (err) {
+                  console.log("Error Found:", err);
+                }
+                console.log('скопирован: ' + file.name)
+              }
+            );
+          }
+        });
+      }
+    }
+  );
+});
